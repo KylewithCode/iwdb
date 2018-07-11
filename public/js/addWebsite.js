400e76fe0633ae9779616cb3f05433a7fcb155df
Dylan <dylanalexandermcdougall@gmail.com>
2018-07-11 09:55:02 -0400

    addWebsite.js will now check if all required fields are filled in before submitting AJAX Request

--- public/js/addWebsite.js
+++ public/js/addWebsite.js
@@ -4,13 +4,19 @@ $('button').on('click', function () {
   var newDesc = $('#description').val();
   var site = {title: newTitle, link: newLink, description: newDesc};
 
-  $.ajax({
-    type: 'POST',
-    url: '/new',
-    data: site,
-    success: function (data) {
-      alert("Website was successfully submitted.");
-      window.location.replace('/');
-    }
-  })
+  console.log(newTitle);
+  if (newTitle === "") alert('A Website Title is required');
+  else if (newLink === "") alert('A Website URL is required');
+  else if (newDesc === "") alert('A Website Description is required');
+  else {
+    $.ajax({
+      type: 'POST',
+      url: '/new',
+      data: site,
+      success: function (data) {
+        alert("Website was successfully submitted.");
+        window.location.replace('/');
+      }
+    })
+  }
 })
