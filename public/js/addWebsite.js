60fa739356ebf1d0404a2ab7a0a8eee5891e0cbc
Dylan <dylanalexandermcdougall@gmail.com>
2018-07-11 09:42:33 -0400

    Removed log that tells if file is linked. UNTESTED: Added JQuery AJAX request to JS file on button click

--- public/js/addWebsite.js
+++ public/js/addWebsite.js
@@ -1,1 +1,16 @@
-console.log('JS File Linked');
+$('button').on('click', function () {
+  var newTitle = $('#title').val();
+  var newLink = $('#link').val();
+  var newDesc = $('#description').val();
+  var site = {title: newTitle, link: newLink, description: newDesc};
+
+  $.ajax({
+    type: 'POST',
+    url: '/new',
+    data: site,
+    success: function (data) {
+      alert("Website was successfully submitted.");
+      window.location.replace('/');
+    }
+  })
+})
