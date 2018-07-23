23189eb39f774d0f9e17c9ad87b93a1db619e519
Dylan McDougall <dylanalexandermcdougall@gmail.com>
2018-07-23 15:08:08 -0400

    Reviews use one table now

--- controllers/iwdbController.js
+++ controllers/iwdbController.js
@@ -69,9 +69,15 @@ module.exports = function(app, db, passport) {
       if (err) throw err;
       console.log(results[req.params.id].title); //Logs title of website with :id
       //turns title into table name: Replaces all spaces with underscores and adds '_reviews' to the end.
-      var table = tableify(results[req.params.id].title);
+
+      // var table = tableify(results[req.params.id].title);
+      // console.log(table);
+      // var sql2 =  `SELECT * FROM ${table};`;
+
+      var table = 'reviews';
       console.log(table);
-      var sql2 =  `SELECT * FROM ${table};`;
+      var sql2 =  `SELECT * FROM reviews WHERE websiteID=${results[req.params.id].id}`;
+
       console.log(sql2);
       db.query(sql2, (err, results2) => { //Second database query (Within first query)
         console.log(results2);
@@ -120,25 +126,26 @@ module.exports = function(app, db, passport) {
   app.post('/add-new-site', urlencodeParser, function (req, res) {
 
     var item = req.body;
-    var table = tableify(item.title);
+    // var table = tableify(item.title);
     var title = fixSqlQuotes(item.title);
     var description = fixSqlQuotes(item.description);
     var link = fixSqlQuotes(item.link);
 
     var sql1 = `INSERT INTO websites (title,description,link) ` +
       `VALUES ("${title}","${description}","${link}");`
-    var sql2 = `CREATE TABLE ${table} ` +
-      `(id int AUTO_INCREMENT NOT NULL, rating int, title varchar(255), review text, malicious varchar(3), originalPoster varchar(255), PRIMARY KEY(id));`
-    console.log(sql1);
-    console.log(sql2);
+    // var sql2 = `CREATE TABLE ${table} ` +
+    //   `(id int AUTO_INCREMENT NOT NULL, rating int, title varchar(255), review text, malicious varchar(3), originalPoster varchar(255), PRIMARY KEY(id));`
+    // console.log(sql1);
+    // console.log(sql2);
 
     db.query(sql1, (err, results) => {
       if (err) throw err;
-      db.query(sql2, (err, results) => {
-        if (err) throw err;
-        console.log(results);
-        res.json(results);
-      })
+      // db.query(sql2, (err, results) => {
+      //   if (err) throw err;
+      //   console.log(results);
+      //   res.json(results);
+      // })
+      res.json(results);
     })
   });
 
@@ -153,7 +160,7 @@ module.exports = function(app, db, passport) {
   });
   app.delete('/remove-site/:title/:id', function (req,res) {
     var sql1 = `DELETE FROM websites WHERE id=${req.params.id}`
-    var sql2 = `DROP TABLE ${tableify(req.params.title)}`
+    var sql2 = `DELETE FROM reviews WHERE websiteID=${req.params.id}`
     db.query(sql1, (err, results) => {
       if (err) throw err;
       db.query(sql2, (err, results) => {
@@ -167,7 +174,14 @@ module.exports = function(app, db, passport) {
     res.render('addReview', {title: req.params.title, user: req.user});
   });
   app.post('/add-review/:title', isLoggedIn, urlencodeParser, function (req,res) {
-    if (true) { //Create SQL command
+
+    //Get website ID
+    var getSiteId = `SELECT id FROM websites WHERE title="${req.params.title}";`
+    db.query(getSiteId, (err, getIdResults) => {
+      if (err) throw err;
+      var siteId = getIdResults[0].id
+
+      //Create SQL command
       var item = req.body;
       var fields = `(title`;
       var values = `("${fixSqlQuotes(item.title)}"`;
@@ -178,20 +192,21 @@ module.exports = function(app, db, passport) {
         fields += `,review`
         values += `,"${fixSqlQuotes(item.review)}"`
       }
-      fields += `,malicious, originalPoster)`
-      values += `,"${item.malicious}", "${req.user.username}")`
+      fields += `,malicious, originalPoster, websiteID)`
+      values += `,"${item.malicious}", "${req.user.id}", "${siteId}")`
 
-      var sql = `INSERT INTO ${tableify(req.params.title)} ${fields} VALUES ${values};`
+      var sql = `INSERT INTO reviews ${fields} VALUES ${values};`
       console.log(sql);
-    }
-    db.query(sql, (err,results) => {
-      if (err) throw err;
-      res.json(results);
-    })
+
+      db.query(sql, (err,results) => {
+        if (err) throw err;
+        res.json(results);
+      })
+    });
   });
 
   app.delete('/delete-review/:title/:id', isLoggedIn, function (req,res) {
-    var sql = `DELETE FROM ${tableify(req.params.title)} WHERE id=${req.params.id};`
+    var sql = `DELETE FROM reviews WHERE id=${req.params.id};`
     console.log(sql);
     db.query(sql, (err, results) => {
       if (err) throw err;
@@ -201,8 +216,7 @@ module.exports = function(app, db, passport) {
 
   app.post('/edit-review/:title/:id', isLoggedIn, urlencodeParser, function (req,res) {
     var item = req.body;
-    var tableName = tableify(req.params.title);
-    sql = `UPDATE ${tableName} SET rating=${item.rating}, title="${item.title}", review="${item.review}" ` +
+    sql = `UPDATE reviews SET rating=${item.rating}, title="${item.title}", review="${item.review}" ` +
       `WHERE id=${req.params.id};`;
     db.query(sql, (err,results) => {
       if (err) throw err;
